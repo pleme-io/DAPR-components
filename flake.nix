3f66add86d501a98f0f9e414002b6fa5f8e201f4
drzln <luis.d@akeyless.io>
2026-06-08 17:41:22 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Dapr components-contrib - reusable components for building distributed applications";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
